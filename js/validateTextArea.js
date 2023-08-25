d5f367efa0f1908f0a27c416b7ad30ddf90cbc7e
Felipe Souza <felipesouza036@gmail.com>
2023-08-25 15:06:19 -0300

    fix: Correct missed imports in js files

--- js/validateTextArea.js
+++ js/validateTextArea.js
@@ -1,49 +1,62 @@
-import { recoverData, saveData } from "./manageData";
-import { deletePageDataOnSkip } from "./clearDataOnSkip";
+import { recoverData, saveData } from "./manageData.js";
+import { deletePageDataOnSkip } from "./clearDataOnSkip.js";
 const textarea = document.getElementById("custom-textarea");
 const charCounter = document.getElementById("char-counter");
 const nextLink = document.getElementById("next-link");
-nextLink === null || nextLink === void 0 ? void 0 : nextLink.addEventListener("click", finishForm);
+nextLink === null || nextLink === void 0
+  ? void 0
+  : nextLink.addEventListener("click", finishForm);
 recoverTextarea();
 checkTextarea();
 deletePageDataOnSkip("fourthStep");
 if (!textarea.value.length) {
-    nextLink === null || nextLink === void 0 ? void 0 : nextLink.classList.add("button--disabled");
+  nextLink === null || nextLink === void 0
+    ? void 0
+    : nextLink.classList.add("button--disabled");
+} else {
+  nextLink === null || nextLink === void 0
+    ? void 0
+    : nextLink.classList.remove("button--disabled");
 }
-else {
-    nextLink === null || nextLink === void 0 ? void 0 : nextLink.classList.remove("button--disabled");
-}
-textarea === null || textarea === void 0 ? void 0 : textarea.addEventListener("keyup", (e) => {
-    let length = textarea.value.length;
-    if (!length) {
-        nextLink === null || nextLink === void 0 ? void 0 : nextLink.classList.add("button--disabled");
+textarea === null || textarea === void 0
+  ? void 0
+  : textarea.addEventListener("keyup", (e) => {
+      let length = textarea.value.length;
+      if (!length) {
+        nextLink === null || nextLink === void 0
+          ? void 0
+          : nextLink.classList.add("button--disabled");
         charCounter.innerHTML = "0/130";
         return;
-    }
-    else {
-        nextLink === null || nextLink === void 0 ? void 0 : nextLink.classList.remove("button--disabled");
-    }
-    if (length + 1 > 130) {
+      } else {
+        nextLink === null || nextLink === void 0
+          ? void 0
+          : nextLink.classList.remove("button--disabled");
+      }
+      if (length + 1 > 130) {
         const currentValue = textarea.value.slice(0, 130);
         textarea.value = currentValue;
         charCounter.innerHTML = `${currentValue.length}/130`;
         return;
-    }
-    charCounter.innerHTML = `${length}/130`;
-});
+      }
+      charCounter.innerHTML = `${length}/130`;
+    });
 function finishForm() {
-    const data = { value: textarea.value };
-    saveData(data, "fourthStep");
+  const data = { value: textarea.value };
+  saveData(data, "fourthStep");
 }
 function checkTextarea() {
-    if (textarea.value.length)
-        return nextLink === null || nextLink === void 0 ? void 0 : nextLink.classList.remove("button--disabled");
-    return nextLink === null || nextLink === void 0 ? void 0 : nextLink.classList.add("button--disabled");
+  if (textarea.value.length)
+    return nextLink === null || nextLink === void 0
+      ? void 0
+      : nextLink.classList.remove("button--disabled");
+  return nextLink === null || nextLink === void 0
+    ? void 0
+    : nextLink.classList.add("button--disabled");
 }
 function recoverTextarea() {
-    const dataRecovered = recoverData("fourthStep");
-    if (!dataRecovered)
-        return;
-    textarea.value = dataRecovered.value;
-    charCounter.innerHTML = `${dataRecovered.value.length}/130`;
+  const dataRecovered = recoverData("fourthStep");
+  if (!dataRecovered) return;
+  textarea.value = dataRecovered.value;
+  charCounter.innerHTML = `${dataRecovered.value.length}/130`;
 }
